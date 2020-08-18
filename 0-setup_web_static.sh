0e2d407e9cae8a795656d33b1b79d5b1b68f3a37
davehh1211 <davehh1211@gmail.com>
2020-08-18 01:12:22 +0000

    task 0.1

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,12 +3,9 @@
 apt-get -y update
 apt-get -y install nginx
 ufw allow 'Nginx HTTP'
-# Create the folder /data/web_static/releases/test/ if it doesn’t already exist
 sudo mkdir -p /data/web_static/releases/test/
-# Create the folder /data/web_static/shared/ if it doesn’t already exist
 sudo mkdir -p /data/web_static/shared/
 
-# Create a fake HTML file /data/web_static/releases/test/index.html (with simple content, to test your Nginx configuration)
 echo "<html>
     <head>
     </head>
@@ -16,12 +13,9 @@ echo "<html>
         Holberton School
     </body>
 </html>" > /data/web_static/releases/test/index.html
-# reate a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder. If the symbolic link already exists, it should be deleted and recreated every time the script is ran.
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-# Give ownership of the /data/ folder to the ubuntu user AND group (you can assume this user and group exist). This should be recursive; everything inside should be created/owned by this user/group.
-chown -R ubuntu:ubuntu /data/
 
-# Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static (ex: https://mydomainname.tech/hbnb_static). Don’t forget to restart Nginx after updating the configuration:
-sed -i "location /hbnb_static/ { alias /data/web_static/current/; }"
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+sed -i "location /hbnb_static/ { alias /data/web_static/current/; }" /etc/nginx/sites-available/default
 
 service nginx restart
