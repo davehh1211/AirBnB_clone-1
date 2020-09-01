c48d7c103a55f7d8c66a8c71fd193e6933ef17be
davehh1211 <davehh1211@gmail.com>
2020-09-01 21:28:10 +0000

    task 5.0

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -41,7 +41,7 @@ def integer(n):
     return '{:d} is a number'.format(n)
 
 
-@app.route('/number_template/<n>', strict_slashes=False)
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def numbertemplate(n):
     """[summary]"""
     return render_template('5-number.html', n=n)
