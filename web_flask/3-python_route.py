e127379761ab227756b9e78ee4d5829f88c25e9b
davehh1211 <davehh1211@gmail.com>
2020-09-01 20:13:05 +0000

    task 3.0

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""displays a default template
+    """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def appflask():
+    """ displays the path by default"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def index2():
+    """displays another route"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cdisplay(text):
+    """displays C is <text>"""
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python/(<text>)', strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
+def pythonfun(text='is cool'):
+    """[summary]
+    Args:
+    text ([type]): [description]
+    """
+    return 'Python ' + text.replace('_', ' ')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
